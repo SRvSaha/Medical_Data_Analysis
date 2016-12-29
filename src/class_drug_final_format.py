633d180defed8b7d51feb46b3e82f94b8f0b2c56
SRvSaha <contact.srvsaha@gmail.com>
2016-12-29 19:00:16 +0530

    Added ENROLL_ID, Removed CATAGORY

--- src/class_drug_final_format.py
+++ src/class_drug_final_format.py
@@ -37,8 +37,8 @@ if len(sys.argv) == 4:
     f_opana = open("opana_final_format.csv", 'w')
     with open(sys.argv[3], 'r') as f:
         codes_opana = f.readline().strip().split(',')
-    with open(sys.argv[3], 'r') as f:
-        codes_opana = f.readline().strip().split(',')
+    with open(sys.argv[2], 'r') as f:
+        codes_butrans = f.readline().strip().split(',')
     with open(sys.argv[1]) as f:
         for line in f.readlines()[1:]:
             line_splitted = line.strip().split(',')
@@ -48,39 +48,43 @@ if len(sys.argv) == 4:
                 #           ','.join(line[6:-3]) + '\n')
                 opana.add(','.join(line))
             else:
-                with open(sys.argv[2], 'r') as f:
-                    codes_butrans = f.readline().strip().split(',')
                 line = [item for item in line_splitted if item != '']
                 # butrans.add(','.join(line[:2]) + ',' +
                 #             ','.join(line[6:-3]) + '\n')
                 butrans.add(','.join(line))
         butrans = sorted(butrans, key=lambda x: int(x.split(',')[0]))
         opana = sorted(opana, key=lambda x: int(x.split(',')[0]))
-    header = 'SEX,AGEGRP,REGION,CATAGORY,REFILL_COUNT,ADMTYP,'
-    f_butrans.write(header + ','.join(codes_butrans) + '\n')
-    f_opana.write(header + ','.join(codes_opana) + '\n')
+    header = 'ENROLL_ID,SEX,AGEGRP,REGION,REFILL_COUNT,ADMTYP,'
+    f_butrans.write(header + ','.join(codes_butrans) + ',Class_Drug\n')
+    f_opana.write(header + ','.join(codes_opana) + ',Class_Drug\n')
     for record in butrans:
         record = record.split(',')
-        output = ','.join(record[1:6]) + ',' + record[-3]
+        # if int(record[-3]) > 5:
+        #     print(record[0],record[-3])
+        # Adding ENROLL_ID and removing CATAGORY
+        output = ','.join(record[:4]) + ',' + record[5] + ',' + record[-3]
         for code in codes_butrans:
             if code in record[6:-3]:
                 output += ',1'
             else:
                 output += ',0'
-        f_butrans.write(output + '\n')
+        f_butrans.write(output + ',butrans\n')
     for record in opana:
         record = record.split(',')
-        output = ','.join(record[1:6]) + ',' + record[-3]
+        # if int(record[-3]) > 5:
+        #     print(record[-3])
+        output = ','.join(record[:4]) + ',' + record[5] + ',' + record[-3]
         for code in codes_opana:
             if code in record[6:-3]:
                 output += ',1'
             else:
                 output += ',0'
-        f_opana.write(output + '\n')
+        f_opana.write(output + ',opana\n')
     f_butrans.close()
     f_opana.close()
     print("Operation Successful :)")
 else:
     print("Argument(s) missing.\n\
-Please run the code as python3 class_drug_final_format.py DATASET.csv RULES.csv")
+Please run the code as python3 class_drug_final_format.py DATASET.csv \
+RULES.csv")
     sys.exit(1)
